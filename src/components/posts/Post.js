044437ad55515d521bd129fb115c4ccc0323a93e
yasmin203 <anayasoo2031995@gmail.com>
2022-08-29 18:28:29 +0300

    useStat and useEffect hooks

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -1,41 +1,25 @@
-import React from 'react'
-import img1 from '../../images/product1.jpg'
-import img2 from '../../images/product3.jpg'
-import styles from '../styles.css'
- const Post = () => {
+import React from "react"
+import img1 from "../../images/product1.jpg"
+import img2 from "../../images/product3.jpg"
+import styles from "../styles.css"
+const Post = ({post,handleDelete}) => {
+  // const Post = ({post}) => {
+  // console.log(props)
   return (
-        <div className="second-container">
-        <div className='img-container'>
-            <img src={img1} alt="post-img" />
-        </div>
-        <div className="post-content">
-            <span>Post Title 1</span>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas iure ducimus id eaque fugiat deleniti qui laudantium minus dolore itaque voluptates dolorem neque quibusdam cum quisquam, dignissimos nesciunt facere magnam!</p>
+    <div className='second-container'>
+      <div className='img-container'>
+        <img src={post.image} alt='post-img' />
+      </div>
+      <div className='post-content'>
+        <span>{post.title}</span>
+        <h6>By: {post.author? post.author:"yasmin"}</h6>
+        <p>
+          {post.description}
+        </p>
+        <button className="btn" onClick={()=>handleDelete(post.id)}>Delete</button>
 
-            </div>
-</div>
-
-    //     <div className='img-container'>
-    //         <img src={img1} alt="post-img" />
-    //         <div className="post-content">
-    //         <span>Post Title 1</span>
-    //         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas iure ducimus id eaque fugiat deleniti qui laudantium minus dolore itaque voluptates dolorem neque quibusdam cum quisquam, dignissimos nesciunt facere magnam!</p>
-
-    //         </div>
-    //     </div>
-
-
-
-    //     <div className='img-container'>
-    //         <img src={img2} alt="post-img" />
-    //         <div className="post-content">
-    //         <span>Post Title 2</span>
-    //         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas iure ducimus id eaque fugiat deleniti qui laudantium minus dolore itaque voluptates dolorem neque quibusdam cum quisquam, dignissimos nesciunt facere magnam!</p>
-
-    //         </div>
-    //     </div>
-    // </div>
-   
+      </div>
+    </div>
   )
 }
 export default Post
--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,17 +1,31 @@
-import React from 'react'
-import Post from './Post'
- const Posts = () => {
+import React, { useState,useEffect } from "react"
+import Post from "./Post"
+
+const Posts = () => {
+  const [posts, setPosts] = useState(null)
+   useEffect(()=>{
+fetch("http://localhost:4000/posts").then(res=>{
+  return res.json()
+}).then(data=>{
+  console.log(data)
+})
+   },[])
+  const handleDelete = (id) => {
+    let currposts = [...posts]
+    let newPosts = currposts.filter((post) => post.id !== id)
+    setPosts(newPosts)
+
+    // console.log(newPosts)
+  }
+
   return (
     <div className='container'>
-    <div className='single-container'>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
+      <div className='single-container'>
+        {posts ? posts.map((post) => (
+          <Post key={post.id} post={post} handleDelete={handleDelete} />
+        )):"no posts"}
+      </div>
     </div>
-    </div>
-  
   )
 }
 export default Posts
